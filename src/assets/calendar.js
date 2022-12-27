93a1beff876813b23d46b93242367a90b8b58a29
Dmytro Hirich <diman12345677@gmail.com>
2022-12-27 11:27:10 +0200

    init

---
+++ src/assets/calendar.js
@@ -0,0 +1,538 @@
+export const CALENDAR = [
+    {
+        month: "January 2023",
+        days: [
+            {
+                day: 1, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 2, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 3, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 4, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 5, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 6, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 7, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 8, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 9, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 10, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 11, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 12, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 13, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 14, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 15, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 16, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 17, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 18, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 19, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 20, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 21, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 22, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 23, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 24, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 25, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 26, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 27, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 28, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 29, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+            {
+                day: 30, 
+                timeToSchedule: {
+                    "10:00": false,
+                    "12:00": false,
+                    "14:00": false,
+                    "16:00": false,
+                },
+            },
+        ],
+        index: 1,
+    },
+    // {
+    //     month: "January 2023",
+    //     days: [
+    //         {
+    //             day: 1, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 2, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 3, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 4, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 5, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 6, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 7, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 8, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 9, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 10, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 11, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 12, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 13, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 14, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 15, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 16, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 17, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 18, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 19, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 20, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 21, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 22, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 23, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 24, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 25, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 26, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 27, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //         {
+    //             day: 28, 
+    //             timeToSchedule: {
+    //                 "10:00": false,
+    //                 "12:00": false,
+    //                 "14:00": false,
+    //                 "16:00": false,
+    //             },
+    //         },
+    //     ],
+    //     index: 2,
+    // },
+]
+
+export const timeToSchedule = ["10:00", "12:00", "14:00", "16:00",]
